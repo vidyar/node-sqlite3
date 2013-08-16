aba38e289d088b900a2def7acffdf15e5b8168c4
Dane Springmeyer <dane@dbsgeo.com>
2013-08-16 18:14:05 -0400

    clear out original binary, then copy after built

--- bin/travis.sh
+++ bin/travis.sh
@@ -6,13 +6,11 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   git config --global user.name "Travis"
   git clone --quiet --branch=${TRAVIS_BRANCH} https://${GH_TOKEN}@github.com/developmentseed/node-sqlite3.git node-sqlite3-bin > /dev/null
   cd node-sqlite3-bin
-  ls ./bin/*
-  rm -rf ./bin/linux-*
-  npm install
-  #./bin/remake.sh
-  ls ./bin/*
-  git add ./bin/*
-  git commit -a -m "update binaries for linux"
+  BINARY=$(node -e "console.log(process.platform +'-'+process.arch+'-v8-'+(/[0-9]+\.[0-9]+/.exec(process
+.versions.v8)[0]))")
+  cp ${TRAVIS_BUILD_DIR}/bin/$BINARY/*node ./bin/${BINARY}/
+  git add ./bin/${BINARY}/*node
+  git commit -a -m "update ${BINARY} binary"
   #git remote rm origin
   #git remote add origin https://@github.com/developmentseed/node-sqlite3.git
   git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} pushed to ${TRAVIS_BRANCH} [ci skip]"
