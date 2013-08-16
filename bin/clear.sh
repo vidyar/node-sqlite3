5cb356b33a1dda4c9f45015a4b2050ff957c59fd
Dane Springmeyer <dane@dbsgeo.com>
2013-08-16 18:33:59 -0400

    fix typo

--- bin/clear.sh
+++ bin/clear.sh
@@ -1,8 +1,9 @@
 BINARY=$(node -e "console.log(process.platform +'-'+process.arch+'-v8-'+(/[0-9]+\.[0-9]+/.exec(process
 .versions.v8)[0]))")
 BINARY_FILE="./bin/${BINARY}/node_sqlite3.node"
-if [ -f ${BINARY_FILE} ]; then
-    rm "Removing: ${BINARY_FILE}"
+if [ -f "${BINARY_FILE}" ]; then
+    echo "Removing: ${BINARY_FILE}"
+    rm "${BINARY_FILE}"
 else
     echo "Not found: ${BINARY_FILE}"
 fi
