7800332a52d95713123fc9b6c75e72ce6d7aef67
Dane Springmeyer <dane@dbsgeo.com>
2013-08-16 18:16:21 -0400

    avoid double commit

--- bin/travis.sh
+++ bin/travis.sh
@@ -1,6 +1,5 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "rebuilding binaries for node v${TRAVIS_NODE_VERSION}\n"
-  echo `pwd`
   cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
@@ -10,10 +9,7 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 .versions.v8)[0]))")
   cp ${TRAVIS_BUILD_DIR}/bin/$BINARY/*node ./bin/${BINARY}/
   git add ./bin/${BINARY}/*node
-  git commit -a -m "update ${BINARY} binary"
-  #git remote rm origin
-  #git remote add origin https://@github.com/developmentseed/node-sqlite3.git
-  git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} pushed to ${TRAVIS_BRANCH} [ci skip]"
+  git commit -a -m "Travis build ${TRAVIS_BUILD_NUMBER}: update ${BINARY} binary in ${TRAVIS_BRANCH} [ci skip]"
   git push -fq origin ${TRAVIS_BRANCH} > /dev/null
   echo "Finished posting binaries\n"
 fi
