d17f84aa89ffe2cf2f3a3cc559289dd8fa03e93e
Dane Springmeyer <dane@dbsgeo.com>
2013-08-16 18:00:02 -0400

    fresh checkout approach

--- bin/travis.sh
+++ bin/travis.sh
@@ -1,18 +1,21 @@
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   echo "rebuilding binaries for node v${TRAVIS_NODE_VERSION}\n"
   echo `pwd`
-  #cd $HOME
+  cd $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis"
-  #git clone --quiet --branch=${TRAVIS_BRANCH} https://${GH_TOKEN}@github.com/developmentseed/node-sqlite3.git > /dev/null
-  #cd node-sqlite3
-  #npm install
+  git clone --quiet --branch=${TRAVIS_BRANCH} https://${GH_TOKEN}@github.com/developmentseed/node-sqlite3.git node-sqlite3-bin > /dev/null
+  cd node-sqlite3-bin
+  ls ./bin/*
+  rm -rf ./bin/linux-*
+  npm install
   #./bin/remake.sh
-  git add bin/*
+  ls ./bin/*
+  git add ./bin/*
   git commit -a -m "update binaries for linux"
-  git remote rm origin
-  git remote add origin https://@github.com/developmentseed/node-sqlite3.git
-  git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} pushed to pre-built"
-  git push -fq origin pre-built > /dev/null
+  #git remote rm origin
+  #git remote add origin https://@github.com/developmentseed/node-sqlite3.git
+  git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} pushed to ${TRAVIS_BRANCH} [ci skip]"
+  git push -fq origin ${TRAVIS_BRANCH} > /dev/null
   echo "Finished posting binaries\n"
 fi
